0c148cf7c192d7b367e27fb82ee7bcf3ecb31163
Stoyan Haydushki <s.haidushky@abv.bg>
2019-10-06 16:55:19 +0300

    Resolve merge conflict
    Conflict file: Year 2/Module 5 -
    OOP/Iterators-Comparators-Reflection/Stealer/Spy.cs

--- Year 2/Module 5 - OOP/Iterators-Comparators-Reflection/Stealer/Spy.cs
+++ Year 2/Module 5 - OOP/Iterators-Comparators-Reflection/Stealer/Spy.cs
@@ -13,19 +13,11 @@ namespace Stealer
 
             var type = typeof(Hacker);
             var hackerInstance = Activator.CreateInstance(type);
-<<<<<<< HEAD
-=======
-.000000000.0
->>>>>>> 66c5fe4852bf453936367f34110c7fd785447eac
             for (int i = 0; i < inputFields.Length; i++)
             {
                 var currentField = type.GetField(inputFields[i], BindingFlags.Public |
                  BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
-<<<<<<< HEAD
                 var value = currentField.GetValue();
-=======
-                var value = currentField.GetValue()
->>>>>>> 66c5fe4852bf453936367f34110c7fd785447eac
             }
 
         }
