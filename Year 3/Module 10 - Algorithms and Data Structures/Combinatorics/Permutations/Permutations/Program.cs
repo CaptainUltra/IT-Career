98fd1f680598429099fe90b2f16f64097e2add1a
Stoyan Haydushki <s.haidushky@abv.bg>
2020-09-04 13:05:30 +0300

    Module10: Combinatorics - Permutation Exercises

---
+++ Year 3/Module 10 - Algorithms and Data Structures/Combinatorics/Permutations/Permutations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Permutations
+{
+    class Program
+    {
+        static int[] elements;
+        static bool[] used;
+        static int[] permutations;
+        public static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        permutations[index] = elements[i];
+                        Permute(index + 1);
+                        used[i] = false;
+                    }
+            }
+        }
+        static void Main(string[] args)
+        {
+            elements = new int[] {1, 2, 3};
+            used = new bool[elements.Length];
+            permutations = new int[elements.Length];
+            Permute(0);
+        }
+    }
+}
---
+++ Year 3/Module 10 - Algorithms and Data Structures/Combinatorics/Permutations/PermutationsOptimized/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PermutationsOptimized
+{
+    class Program
+    {
+        static int[] elements;
+        static void Permute(int index)
+        {
+            if(index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+            }
+            else
+            {
+                Permute(index + 1);
+                for (int i = index + 1; i < elements.Length; i++)
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+                }
+            }
+        }
+        static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+        static void Main(string[] args)
+        {
+            elements = new int[] {1, 2, 3};
+            Permute(0);
+        }
+    }
+}
