61fc2a1418fd2c434da9ce9da6afd90bb076d19f
Stoyan Haydushki <s.haidushky@abv.bg>
2020-09-07 14:20:04 +0300

    Combinatorics, M10: Add combinations & variations

---
+++ Year 3/Module 10 - Algorithms and Data Structures/Combinatorics/Combinations/CombinationsWithRepetition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CombinationsWithRepetition
+{
+    class Program
+    {
+        static int k;
+        static int[] numbers;
+        static int[] combinations;
+        static void Combine(int index, int start)
+        {
+            if (index >= k)
+            {
+                Console.WriteLine(string.Join(", ", combinations));
+                return;
+            }
+            for (int i = start; i < numbers.Length; i++)
+            {
+                combinations[index] = numbers[i];
+                Combine(index + 1, i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            numbers = new int[] { 1, 2, 3, 4 };
+            k = 2;
+            combinations = new int[k];
+            Combine(0, 0);
+        }
+    }
+}
---
+++ Year 3/Module 10 - Algorithms and Data Structures/Combinatorics/Variations/Variations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Variations
+{
+    class Program
+    {
+        static int k;
+        static int[] numbers;
+        static bool[] used;
+        static int[] variations;
+        public static void Generate(int index)
+        {
+            if (index >= k)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    variations[index] = numbers[i];
+                    Generate(index + 1);
+                    used[i] = false;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            numbers = new int[] { 1, 2, 3, 4 };
+            used = new bool[numbers.Length];
+            k = 2;
+            variations = new int[k];
+            Generate(0);
+        }
+    }
+}
