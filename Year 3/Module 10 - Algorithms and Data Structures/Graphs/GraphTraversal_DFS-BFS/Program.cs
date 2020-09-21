35aad1bad8e623f46776055f28625ef3476c9058
Stoyan Haydushki <s.haidushky@abv.bg>
2020-09-21 11:20:02 +0300

    Module 10 update: Graphs
    Traversal with BFS and DFS algorithms and Topological sort

---
+++ Year 3/Module 10 - Algorithms and Data Structures/Graphs/GraphTraversal_DFS-BFS/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphTraversal_DFS_BFS
+{
+    //Different way of representing a graph with class Node
+    /*class Node
+    {
+        public int Id { get; set; }
+        public List<int> Children { get; set; }
+    }*/
+    class Program
+    { 
+        static List<int>[] graph;
+        static bool[] visited;
+
+        static void Dfs(int n)
+        {
+            if (!visited[n])
+            {
+                visited[n] = true;
+                foreach (var child in graph[n])
+                { 
+                    Dfs(child);
+                }
+                Console.Write($"{n} ");
+            }
+        }
+        static void Bfs(int n)
+        {
+            if (visited[n])
+            {
+                return;
+            }
+            var queue = new Queue<int>();
+            queue.Enqueue(n);
+            visited[n] = true;
+
+            while (queue.Count !=0)
+            {
+                var current = queue.Dequeue();
+                Console.WriteLine(current);
+
+                foreach (var child in graph[current])
+                {
+                    if (!visited[child])
+                    {
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            //Different way of representing a graph with class Node
+            //graph = new List<Node>;
+
+            graph = new List<int>[]
+            {
+                new List<int> {3, 6},
+                new List<int> {2, 3, 4, 5, 6},
+                new List<int> {1, 4, 5},
+                new List<int> {0, 1, 4},
+                new List<int> {1, 2, 6},
+                new List<int> {1, 2, 3},
+                new List<int> {0, 1, 4},
+                new List<int> {8},
+                new List<int> {7}
+            };
+
+            visited = new bool[graph.Length];
+            for (int i = 0; i < graph.Length; i++)
+            {
+                //Dfs(i);
+                Bfs(i);
+            }
+
+            //Connected components
+            /*var components = 0;
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if(!visited[i])
+                {
+                    components++;
+                    Console.WriteLine($"Connected component {components}: ");
+                    Dfs(i);
+                    Console.WriteLine();
+                }
+            }*/
+        }
+    }
+}
---
+++ Year 3/Module 10 - Algorithms and Data Structures/Graphs/TopologicalSorting/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopologicalSorting
+{
+    class Program
+    {
+        static List<int>[] graph;
+        static HashSet<int> GetNodesWithIncomingEdges()
+        {
+            var nodeWithIncomingEdges = new HashSet<int>();
+            graph.SelectMany(s => s)
+                .ToList()
+                .ForEach(e => nodeWithIncomingEdges.Add(e));
+            return nodeWithIncomingEdges;
+        }
+        static void Main(string[] args)
+        {
+            graph = new List<int>[]
+            {
+                new List<int> {1,2},    //A: B, C
+                new List<int> {3,4},    //B: D, E
+                new List<int> {5},      //C: F  
+                new List<int> {2,5},    //D: C, F
+                new List<int> {3},      //E: D
+                new List<int> { },      //F: 
+            };
+
+            var result = new List<int>();
+            var nodes = new HashSet<int>();
+
+            var nodesWithIncomingEdges = GetNodesWithIncomingEdges();
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (!nodesWithIncomingEdges.Contains(i))
+                {
+                    nodes.Add(i);
+                }
+            }
+            while (nodes.Count !=0)
+            {
+                var currentNode = nodes.First();
+                nodes.Remove(currentNode);
+
+                result.Add(currentNode);
+                var children = graph[currentNode].ToList();
+                graph[currentNode] = new List<int>();
+
+                var leftNodesWithIncomingEdges = GetNodesWithIncomingEdges();
+                
+                foreach (var child in children)
+                {
+                    if (!leftNodesWithIncomingEdges.Contains(child))
+                    {
+                        nodes.Add(child);
+                    }    
+                }
+            }
+            if(graph.SelectMany(s=>s).Any())
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",result));
+            }
+        }
+    }
+}
