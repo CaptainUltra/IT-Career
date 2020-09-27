41077b7c1d9672e15de3bd3678ef5f4379682aa3
Stoyan Haydushki <s.haidushky@abv.bg>
2020-09-27 20:29:47 +0300

    Module 10: Graphs update

---
+++ Year 3/Module 10 - Algorithms and Data Structures/Graphs/DijkstraAlgorithm/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DijkstraAlgorithm
+{
+    class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+
+    class Program
+    {
+        static Dictionary<int, List<Edge>> nodeToEdges = new Dictionary<int, List<Edge>>();
+
+        static void Main(string[] args)
+        {
+            var graph = new List<Edge>()
+            {
+                new Edge {First = 2, Second = 4, Weight = 2},
+                new Edge {First = 1, Second = 2, Weight = 4},
+                new Edge {First = 1, Second = 3, Weight = 5},
+                new Edge {First = 8, Second = 9, Weight = 7},
+                new Edge {First = 1, Second = 4, Weight = 9},
+                new Edge {First = 4, Second = 5, Weight = 8},
+                new Edge {First = 5, Second = 6, Weight = 12},
+                new Edge {First = 7, Second = 8, Weight = 8},
+                new Edge {First = 7, Second = 9, Weight = 10},
+                new Edge {First = 3, Second = 5, Weight = 7},
+                new Edge {First = 3, Second = 4, Weight = 20},
+            };
+
+            var nodes = graph.Select(e => e.First)
+                    .Union(graph.Select(e => e.Second))
+                    .Distinct()
+                    .OrderBy(e => e)
+                    .ToHashSet();
+
+            foreach (var edge in graph)
+            {
+                if(!nodeToEdges.ContainsKey(edge.First))
+                {
+                    nodeToEdges[edge.First] = new List<Edge>();
+                }
+                if (!nodeToEdges.ContainsKey(edge.Second))
+                {
+                    nodeToEdges[edge.Second] = new List<Edge>();
+                }
+                nodeToEdges[edge.First].Add(edge);
+                nodeToEdges[edge.Second].Add(edge);
+            }
+
+            var distances = new int[nodes.Max() + 1];
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = int.MaxValue;
+            }
+
+            distances[nodes.First()] = 0;
+            var queue = new SortedSet<int>(Comparer<int>.Create((f, s) => distances[f] - distances[s]));
+
+            queue.Add(nodes.First());
+            while (queue.Count != 0)
+            {
+                var min = queue.Min;
+                queue.Remove(min);
+
+                foreach (var edge in nodeToEdges[min])
+                {
+                    var otherNode = edge.First == min ? edge.Second : edge.First;
+                    if(distances[otherNode] < int.MaxValue)
+                    {
+                        queue.Add(otherNode);
+                    }
+
+                    var newDistance = distances[min] + edge.Weight;
+                    if(newDistance < distances[otherNode])
+                    {
+                        distances[otherNode] = newDistance;
+
+                        //Trigger sorting
+                        queue.Remove(otherNode);
+                        queue.Add(otherNode);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Year 3/Module 10 - Algorithms and Data Structures/Graphs/DistanceBetweenNodes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DistanceBetweenNodes
+{
+    class Program
+    {
+        static Dictionary<int, List<int>> graph;
+        static Dictionary<int, bool> visited;
+        static int minCount;
+
+        static void Main(string[] args)
+        {
+            graph = new Dictionary<int, List<int>>();
+            int nodeCount = int.Parse(Console.ReadLine());
+            int pathsToFindCount = int.Parse(Console.ReadLine());
+
+            //Nodes input
+            for (int i = 0; i < nodeCount; i++)
+            {
+
+                var input = Console.ReadLine().Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int node = input[0];
+                var connections = input.Skip(1).ToList();
+
+                graph[node] = new List<int>();
+                graph[node].AddRange(connections);
+            }
+
+            //Paths to try inptut
+            for (int i = 0; i < pathsToFindCount; i++)
+            {
+                var nodes = Console.ReadLine().Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                visited = new Dictionary<int, bool>();
+                foreach (var node in graph.Keys)
+                {
+                    visited.Add(node, false);
+                }
+                minCount = graph.Count();
+
+                Path(nodes[0], 0, nodes[1]);
+                if (minCount == graph.Count) minCount = -1;
+                System.Console.WriteLine("{" + nodes[0] + ", " + nodes[1] + "} -> " + minCount);
+            }
+        }
+        static void Path(int current, int counter, int destination)
+        {
+            visited[current] = true;
+            if (current == destination)
+            {
+                if (counter < minCount)
+                {
+                    minCount = counter;
+                    visited[current] = false;
+                    return;
+                }
+            }            
+            foreach (var child in graph[current])
+            {
+                if (!visited[child])
+                {
+                    Path(child, counter + 1, destination);
+                }
+            }
+            visited[current] = false;
+        }
+    }
+}
