2bace052a56f83cd8ed44ade43366aa7b9a37b2d
Stoyan Hayduski <s.haidushky@abv.bg>
2020-09-29 14:21:55 +0300

    Module 10: Greedy algorithms: MagnetTape prblem

---
+++ Year 3/Module 10 - Algorithms and Data Structures/Greedy Algorithms/MagnetTape/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MagnetTape
+{
+    class Programme
+    {
+        public int Number { get; private set; }
+        public int Length { get; private set; }
+        public double Probability { get; private set; }
+        public double ImportanceCoefficient => this.Probability / this.Length;
+        public Programme(int number, int lenght, double probability)
+        {
+            this.Number = number;
+            this.Length = lenght;
+            this.Probability = probability;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            var programmes = new List<Programme>()
+            {
+                new Programme(1, 1, 0.3),
+                new Programme(2, 3, 0.1),
+                new Programme(3, 2, 0.5),
+                new Programme(4, 4, 0.2)
+            };
+
+            programmes = programmes.OrderByDescending(x => x.ImportanceCoefficient).ToList();
+
+            foreach (var item in programmes)
+            {
+                Console.Write(item.Number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
