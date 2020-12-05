4d024fc1b693f077737403968bfda4d3a55ba34e
Stoyan Haydushki <s.haidushky@abv.bg>
2020-12-05 16:57:45 +0200

    Module 11: ChatServer - simple sockets exercise

---
+++ Year 3/Module 12 - Internet Programming/ChatServer/ChatClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress iPAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEndPoint = new IPEndPoint(iPAddress, 11000);
+
+            Socket sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(remoteEndPoint);
+                Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+
+                while (true)
+                {
+                    string message = "";
+                    do
+                    {
+                        Console.Write(">");
+                        message = Console.ReadLine();
+                    } while (message == "");
+
+                    byte[] messageData = Encoding.ASCII.GetBytes(message + "<EOF>");
+
+                    int bytesSent = sender.Send(messageData);
+
+                    if(message == "exit")
+                    {
+                        break;
+                    }
+                }
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Year 3/Module 12 - Internet Programming/ChatServer/ChatServer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get host info
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+            IPAddress iPAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress, 11000);
+
+            //Create socket
+            Socket listener = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                byte[] buffer = new byte[1024];
+
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                Socket handle = listener.Accept();
+
+                while (true)
+                {
+                    string message = "";
+
+                    while (true)
+                    {
+                        int messageData = handle.Receive(buffer);
+
+                        message += Encoding.ASCII.GetString(buffer, 0, messageData);
+
+                        if(message.Contains("<EOF>"))
+                        {
+                            message = message.Replace("<EOF>", "");
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine(">" + message);
+
+                    if (message == "exit")
+                    {
+                        handle.Shutdown(SocketShutdown.Both);
+                        handle.Close();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Goodbye!");
+            Console.ReadKey(true);
+        }
+    }
+}
