b7548636250e3e4dee50d46afbab883377687e6c
Stoyan Haydushki <s.haidushky@abv.bg>
2021-02-07 12:36:35 +0200

    Module 12: Exam

---
+++ Year 3/Module 12 - Internet Programming/Exam/ContactsManager/Data/ContactsDb.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ContactsDb : DbContext
+    {
+        public virtual DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ContactsDb;");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
